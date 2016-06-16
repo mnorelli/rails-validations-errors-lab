017c1d09038da82b02ae916ba003c2401dca0377
Michael Norelli <mnorelli360@gmail.com>
2016-06-16 06:14:13 -0700

    first commit, adds validations for owner name, phone, email

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,7 +2,7 @@ class Owner < ActiveRecord::Base
   # TODO: add validations
   validates_presence_of :first_name, :last_name, :email
   validates :first_name, :last_name, :email, length: { maximum: 255 }
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: true }
   validates :phone, presence: true
 
   before_save :normalize_phone_number
